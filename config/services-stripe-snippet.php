59740e1462319f53aff45b5a29af0c8f923ec3d4
Gueye <matar91202@gmail.com>
2026-09-16 01:35:53 +0000

    Bugs sttripes

--- config/services-stripe-snippet.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Extrait a fusionner manuellement dans VOTRE config/services.php existant
-// (ne remplace pas le fichier : ajoutez juste cette entree dans le tableau
-// retourne par le fichier, a cote de 'postmark', 'resend', 'slack', etc.)
-
-'stripe' => [
-    'key' => env('STRIPE_KEY'),
-    'secret' => env('STRIPE_SECRET'),
-],
